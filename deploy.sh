d38b05d937298c97fb5db58d4a8883c55f09c874
Frederik Dudzik <frederik.dudzik@gmail.com>
2016-01-11 01:33:57 +0100

    fix insta link

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -ev
 
 ncftp -u $FTP_USER -p $FTP_PASS $FTP_HOST <<< "
-  put -R ./.tmp
-  mv ./current ./to_delete
-  mv ./.tmp ./current
+  put -R ./.tmp &&
+  mv ./current ./to_delete &&
+  mv ./.tmp ./current &&
   rm -rf ./to_delete"
